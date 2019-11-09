033bd88c3258b05a205501261db912279dfae492
edwiinn <ykedwinh@gmail.com>
2019-11-09 16:50:44 +0700

    Add User model and User repository

--- core/Domain/Model/User/User.php
+++ core/Domain/Model/User/User.php
@@ -3,12 +3,87 @@
 namespace TugasKPL\Domain\Model\User;
 
 class User {
+    /**
+     * @var int
+     */
     protected $userId;
+
+    /**
+     * @var string
+     */
     protected $email;
+    
+    /**
+     * @var string
+     */
     protected $password;
-    protected $createdOn;
-    protected $updatedOn;
+
+    /** 
+     * @var ArrayCollection
+     */
     protected $ideas;
 
-    public function __construct(User)
+    /**
+     * @param int $userId
+     * @param string $email
+     * @param string $password
+     */
+    public function __construct($userId, $email, $password)
+    {
+        $this->userId = $userId;
+        $this->setEmail($email);
+        $this->changePassword($password);
+        $this->ideas = new ArrayCollection();
+    }
+
+    /**
+     * @param string $email
+     */
+    protected function setEmail($email)
+    {
+        $email = trim($email);
+        if (!$email) {
+            throw new \InvalidArgumentException('email');
+        }
+
+        Assertion::email($email);
+        $this->email = strtolower($email);
+    }
+
+    /**
+     * @param string $password
+     */
+    public function changePassword($password)
+    {
+        $password = trim($password);
+        if (!$password) {
+            throw new \InvalidArgumentException('password');
+        }
+
+        $this->password = $password;
+    }
+
+    /**
+     * @return int
+     */
+    public function id()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function email()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function password()
+    {
+        return $this->password;
+    }
 }
