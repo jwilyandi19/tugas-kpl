79487338b828b21866a4b988b119dc0dbe43c019
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-11-09 16:19:34 +0700

    ddd directory structure

---
+++ core/Domain/Model/User/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TugasKPL\Domain\Model\User;
+
+class User {
+    protected $userId;
+    protected $email;
+    protected $password;
+    protected $createdOn;
+    protected $updatedOn;
+    protected $ideas;
+
+    public function __construct(User)
+}
